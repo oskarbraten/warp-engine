222b10099be32aba70f8e45acffe9238766d6e91
oskarbraten <oskar.braten@outlook.com>
2018-11-30 20:44:39 +0100

    Moved example into separate folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,100 @@
 const path = require('path');
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
-    entry: {
-        warp: './src/index.js',
-        demo: './example/demo.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        library: 'Warp'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    failOnWarning: false,
-                    failOnError: true,
+module.exports = [
+    {
+        mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+        entry: {
+            warp: './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+            library: 'warp',
+            libraryTarget: 'umd'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    enforce: 'pre',
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'eslint-loader',
+                    options: {
+                        failOnWarning: false,
+                        failOnError: true,
+                    },
                 },
-            },
-            {
-                test: /\.(glsl|gltf)$/,
-                use: 'raw-loader'
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-                ]
-            }
+                {
+                    test: /\.(glsl|gltf)$/,
+                    use: 'raw-loader'
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin('dist', {}),
         ]
     },
-    plugins: [
-        new CleanWebpackPlugin('dist', {}),
-        new HtmlWebpackPlugin({
-            inject: true,
-            hash: false,
-            template: './example/index.html',
-            filename: 'index.html'
-        })
-    ],
-    stats: "minimal",
-    serve: {
-        dev: {
-            stats: 'minimal'
-        }
+    {
+        mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+        entry: [
+            './example/demo.js',
+            'webpack-plugin-serve/client'
+        ],
+        output: {
+            path: path.resolve(__dirname, 'example/build'),
+            filename: '[name].js'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    enforce: 'pre',
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'eslint-loader',
+                    options: {
+                        failOnWarning: false,
+                        failOnError: true,
+                    },
+                },
+                {
+                    test: /\.(glsl|gltf)$/,
+                    use: 'raw-loader'
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin('example/build', {}),
+            new HtmlWebpackPlugin({
+                inject: true,
+                hash: false,
+                template: './example/index.html',
+                filename: 'index.html'
+            }),
+            new Serve({ static: path.join(process.cwd(), '/example/build') })
+        ],
+        watch: true
     }
-};
+];
