f1f7f50965c7d21ed7805e86d4f03a1cace90a2c
oskarbraten <oskar.braten@outlook.com>
2018-12-28 02:15:38 +0100

    Fix normalMatrix size

--- src/lib/shader/code/pbr-vertex-shader.glsl
+++ src/lib/shader/code/pbr-vertex-shader.glsl
@@ -9,7 +9,7 @@ layout(location = 3) in vec2 in_texcoord_0;
 
 uniform mat4 modelViewProjectionMatrix;
 uniform mat4 modelMatrix;
-uniform mat4 normalMatrix;
+uniform mat3 normalMatrix;
 
 out vec3 position;
 out vec2 texcoord_0;
@@ -21,7 +21,7 @@ void main() {
     vec4 pos = modelMatrix * in_position;
     position = vec3(pos.xyz) / pos.w;
     
-    vec3 normalW = normalize(vec3(normalMatrix * vec4(in_normal.xyz, 0.0)));
+    vec3 normalW = normalize(normalMatrix * in_normal.xyz);
     vec3 tangentW = normalize(vec3(modelMatrix * vec4(in_tangent.xyz, 0.0)));
     vec3 bitangentW = cross(normalW, tangentW) * in_tangent.w;
     TBN = mat3(tangentW, bitangentW, normalW);
